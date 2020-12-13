457c59f1ac34b28e9a2586d54db1336c7be6789c
Assaad Moawad <assaad.moawad@datathings.com>
2020-12-13 17:26:52 +0100

    parser update

---
+++ src/main/java/penrose/parser/Polyline.java
@@ -0,0 +1,2 @@
+package penrose.parser;public class Polyline {
+}
---
+++ src/main/java/penrose/parser/Rectangle.java
@@ -0,0 +1,10 @@
+package penrose.parser;
+
+public class Rectangle extends Shape{
+    double x1;
+    double y1;
+    double x2;
+    double y2;
+
+
+}
